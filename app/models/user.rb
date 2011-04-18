5f8a628f7a712fc5940320286bfcb0a5288fd507
akydd <akydd@ualberta.net>
2011-04-17 23:03:05 -0600

    working on following

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
 
   email_regex =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -49,6 +51,18 @@ class User < ActiveRecord::Base
 
   def feed
     microposts
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
 
   private
