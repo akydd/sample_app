6d08e9d98f09aee7be0950f5e3359944517b7022
Alan Kydd <akydd@ualberta.net>
2012-12-10 20:23:30 -0700

    Moved command classes into lib.  Moved command tests into spec/lib.

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module SampleApp
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    # Added command classes to autoload_paths
+    config.autoload_paths += %W(#{config.root}/lib/commands)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
